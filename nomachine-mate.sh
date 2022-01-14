30412fef9950b81f85da3c1f5689db0f64ea8600
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-01-14 10:22:55 +0700

    Update nomachine-mate.sh

--- nomachine-mate.sh
+++ nomachine-mate.sh
@@ -1,4 +1,4 @@
-wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
+wget -O ng.sh https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine/raw/main/ngrok.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 clear
