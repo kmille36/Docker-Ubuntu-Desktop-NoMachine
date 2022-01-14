4eab3637412ba3fe3ab601b4519b706fa97b9287
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-01-14 10:55:41 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,7 +1,7 @@
 clear
 rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "======================="
-echo "Download ngrok"
+echo "Downloading ngrok..."
 echo "======================="
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
