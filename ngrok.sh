1144d8982efec8b3a61b2d5d944a759eb6f2fc20
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-01-14 10:43:06 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -5,5 +5,6 @@ echo "Download ngrok"
 echo "======================="
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
+echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
 read -p "Paste Ngrok Authtoken: " CRP
 ./ngrok authtoken $CRP 
